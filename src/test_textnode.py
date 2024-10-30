fec977a144b4f3385a0b9809caaf75903c44ad2c
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-29 21:33:23 -0400

    Create tests for TextNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,5 +8,15 @@ class TestTextNode(unittest.TestCase):
     node2 = TextNode("This is a text node", TextType.BOLD)
     self.assertEqual(node, node2)
 
+  def test_still_eq(self):
+    node = TextNode("",TextType.CODE, None)
+    node2 = TextNode("", TextType.CODE, None)
+    self.assertEqual(node, node2)
+
+  def not_eq(self):
+    node = TextNode("This is this",TextType.BOLD, None)
+    node2 = TextNode("This is not that", TextType.CODE, None)
+    self.assertNotEqual(node, node2)
+
 if __name__ == "__main__":
   unittest.main()
