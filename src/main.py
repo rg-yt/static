9e43c904fd689a18cbb9893cd0301f29529a00bb
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-29 21:19:08 -0400

    Update main to create a TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from textnode import *
+
+def main():
+  node = TextNode("texting", NodeType.BOLD, "https://www.boot.dev")
+  node2 = TextNode("testing", NodeType.ITALIC)
+  print(node)
+  print(node2)
+  print(node.__eq__(node2))
+  
+
+main()
