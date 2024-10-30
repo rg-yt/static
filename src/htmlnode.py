80054be80c1d6f312fd9ec45ce98f170be0f9135
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-29 23:18:21 -0400

    Create leaf node subclass

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,7 +9,23 @@ class HTMLNode:
     raise NotImplementedError
   
   def props_to_html(self):
-    return ''.join(f' {key}={value}' for key, value in self.props.items())
+    if self.props == None:
+      return ""
+    return ''.join(f' {key}="{value}"' for key, value in self.props.items())
   
   def __repr__(self):
-      return f"HTML NODE({self.tag}, {self.value}, {self.children}, {self.props})"
+      return f"HTML NODE({self.tag}, {self.value}, children: {self.children}, {self.props})"
+  
+class LeafNode(HTMLNode):
+  def __init__(self, tag, value, props = None):
+    super().__init__(tag, value, None , props)
+
+  def to_html(self):
+    if self.value == None:
+      raise ValueError
+    if self.tag == None:
+      return self.value
+    return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+  
+  def __repr__(self):
+      return f"LEAF NODE({self.tag}, {self.value}, {self.props})"
