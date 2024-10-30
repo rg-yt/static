52d60e88956c2bad59d4e3c0a71dc1048c8804f4
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-29 21:33:46 -0400

    remove testing from main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 from textnode import *
 
 def main():
-  node = TextNode("texting", NodeType.BOLD, "https://www.boot.dev")
-  node2 = TextNode("testing", NodeType.ITALIC)
-  print(node)
-  print(node2)
-  print(node.__eq__(node2))
+  pass
   
 
 main()
